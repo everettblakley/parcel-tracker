57b2b0a881cea0d828e8dc90602b28b81866ccab
Everett Blakley <everettblakley@gmail.com>
2020-09-18 15:58:24 -0600

    More styles for the detail section

--- src/ParcelHistory.svelte
+++ src/ParcelHistory.svelte
@@ -21,21 +21,29 @@
   .items {
     padding: 0 2rem;
     display: flex;
-    overflow-x: scroll;
+    overflow-x: auto;
     overflow-y: hidden;
 
     scrollbar-color: var(--dark-blue) var(--light);
     scrollbar-width: thin;
   }
 
-  .items > div {
+  div.item {
     display: grid;
-    grid-template-rows: 1fr repeat(3, auto);
+    grid-template-rows: 1fr 1fr;
+    flex: 1 0;
     padding: 1rem;
     background: var(--light-grey);
     min-width: 150px;
     word-wrap: break-word;
+    transition: 300ms ease-in-out;
+    color: var(--dark-blue);
   }
+
+  div.item:hover {
+    background: var(--light-blue);
+  }
+
 
   /* div::-webkit-scrollbar {
     width: 1em;
@@ -51,19 +59,25 @@
     outline: 1px solid red;
   } */
 
+  div.details {
+    display: grid;
+    grid-template-rows: repeat(4, auto);
+  }
+
   p.status {
     padding: 8px 0;
-    text-align:center;
     font-size: 14px;
   }
 
   p.date {
     font-size: 10px;
+    align-self: end;
   }
-  
+
   p.time {
     font-size: 10px;
     font-style: italic;
+    align-self: end;
   }
 
   .dot {
@@ -75,7 +89,7 @@
     transition: 200ms ease-in-out;
     cursor: pointer;
   }
-  
+
   .dot:hover {
     background: var(--dark-blue);
     transform: scale(1.5);
@@ -87,14 +101,23 @@
     {#each Object.keys(data) as carrier}
       <h3>Delivered by: {transformCarrierName(carrier)}</h3>
       <div class="items">
-        {#each data[carrier] as item, index}
-          <div>
+        {#each data[carrier] as item}
+          <div class="item">
             <div class="timeline-component">
-              <div class="dot"/>
+              <div class="dot" />
             </div>
-            <p class="status">{item.status}</p>
-            <p class="date">{moment(item.timestamp).format("dddd, MMMM Do YYYY")}</p>
-            <p class="time">{moment(item.timestamp).format("h:mm:ss a")}</p>
+            <div class="details">
+              {#if item.location}
+                <p class="place-name">
+                  {item.location.city}{item.location.state ? ', ' + item.location.state : ''}
+                </p>
+              {/if}
+              <p class="status">{item.status}</p>
+              <p class="date">
+                {moment(item.timestamp).format('dddd, MMMM Do YYYY')}
+              </p>
+              <p class="time">{moment(item.timestamp).format('h:mm:ss a')}</p>
+            </div>
           </div>
         {/each}
       </div>
